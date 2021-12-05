db0a27cc26b0791f1699ff6e81b2fe48c8b2a9c8
Geo Jolly <geojollyc@gmail.com>
2021-12-05 12:57:27 +0530

    Add jdbc connection

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import javax.swing.JOptionPane;
 
 public class Driver {
     public static void main(String[] args) {
+        try {
+            // # jdbc connection
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            String user = "geo";
+            String password = "6023";
+            Class.forName("org.postgresql.Driver");
+            Connection con = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected");
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
         try {
             LOGINPAGE form = new LOGINPAGE();
             form.setSize(500, 400);
