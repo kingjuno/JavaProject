93ff7bd5450c032b0781e64b8e8c864d09e04bc2
Geo Jolly <geojollyc@gmail.com>
2021-12-15 21:42:44 +0530

    Finishing codes

--- src/Home.java
+++ src/Home.java
@@ -4,39 +4,37 @@ import java.awt.event.*;
 import java.sql.Connection;
 import java.util.Hashtable;
 
-
-public class Home extends JFrame implements ActionListener{
-    JPanel Home ;
-    JButton btn_1,btn_2,btn_3, btn_4,btn_5;
-    JLabel Search, home,icon;
-    JTextField Input,Output;
+public class Home extends JFrame implements ActionListener {
+    JPanel Home;
+    JButton btn_1, btn_2, btn_3, btn_4, btn_5;
+    JLabel Search, home, icon;
+    JTextField Input, Output;
     Font font;
     JComboBox<String> cb;
     Connection con;
     String Username;
-    Hashtable<String,String> userdata;
+    Hashtable<String, String> userdata;
 
     Home(Connection con, Hashtable<String, String> userdata) {
         this.con = con;
         this.userdata = userdata;
         setTitle("Music Recording Management System");
-        
-		this.setBounds(100, 100, 564, 408);
+
+        this.setBounds(100, 100, 564, 408);
         Home = new JPanel();
         this.getContentPane().add(Home);
         Home.setLayout(null);
-        Home.setBackground(new Color(68,67,68));
+        Home.setBackground(new Color(68, 67, 68));
 
-        
         icon = new JLabel("");
-		icon.setIcon(new ImageIcon(this.getClass().getResource("assets/background.png")));
-		icon.setBounds(-124, 122, 398, 149);
-		this.getContentPane().add(icon);
+        icon.setIcon(new ImageIcon(this.getClass().getResource("assets/background.png")));
+        icon.setBounds(-124, 122, 398, 149);
+        this.getContentPane().add(icon);
 
         home = new JLabel("HOME");
-        home.setFont(new Font("Tw Cen MT Condensed Extra Bold",Font.PLAIN,25));
+        home.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 25));
         home.setForeground(new Color(240, 255, 255));
-        home.setBounds(250,10,200,50);
+        home.setBounds(250, 10, 200, 50);
         Home.add(home);
 
         btn_1 = new JButton("Add Song");
@@ -44,12 +42,12 @@ public class Home extends JFrame implements ActionListener{
         Home.add(btn_1);
 
         btn_2 = new JButton("View Details");
-		btn_2.setBounds(342, 233, 122, 21);
-		Home.add(btn_2);
-		
-		btn_3 = new JButton("Exit");
-		btn_3.setBounds(484, 340, 56, 21);
-		Home.add(btn_3);
+        btn_2.setBounds(342, 233, 122, 21);
+        Home.add(btn_2);
+
+        btn_3 = new JButton("Exit");
+        btn_3.setBounds(484, 340, 56, 21);
+        Home.add(btn_3);
 
         btn_4 = new JButton("Add Artist");
         btn_4.setBounds(342, 202, 122, 21);
@@ -59,18 +57,27 @@ public class Home extends JFrame implements ActionListener{
         btn_5.setBounds(342, 171, 122, 21);
         Home.add(btn_5);
 
+
         btn_1.addActionListener(this);
-        btn_2.addActionListener(this);
-        btn_3.addActionListener(this);
         btn_4.addActionListener(this);
         btn_5.addActionListener(this);
+
+        btn_2.addActionListener(this);
+        btn_3.addActionListener(this);
+
+        if (userdata.get("role").equals("user")) {
+            btn_1.setEnabled(false);
+            btn_4.setEnabled(false);
+            btn_5.setEnabled(false);
+        }
 
         add(Home, BorderLayout.CENTER);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
     public void actionPerformed(ActionEvent ae) {
-         if (ae.getSource() == btn_1) {
+        if (ae.getSource() == btn_1) {
             this.dispose();
             try {
 
@@ -87,7 +94,7 @@ public class Home extends JFrame implements ActionListener{
             this.dispose();
             try {
 
-                viewDetail form = new viewDetail(con,userdata);
+                viewDetail form = new viewDetail(con, userdata);
                 form.setSize(500, 400);
                 form.setVisible(true);
                 form.invalidate();
@@ -112,8 +119,8 @@ public class Home extends JFrame implements ActionListener{
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(null, e.getMessage());
             }
-         }
-         if (ae.getSource() == btn_5) {
+        }
+        if (ae.getSource() == btn_5) {
             this.dispose();
             try {
                 AlbumInfo form = new AlbumInfo(con, userdata);
@@ -124,7 +131,7 @@ public class Home extends JFrame implements ActionListener{
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(null, e.getMessage());
             }
-         }
+        }
 
     }
 }
--- src/viewDetail.java
+++ src/viewDetail.java
@@ -3,80 +3,69 @@ import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
 import java.util.Hashtable;
-public class viewDetail extends JFrame implements ActionListener{
-    JPanel SongInfoWindow ;
-    JButton b,back;
-    JLabel Search,Back;
+
+public class viewDetail extends JFrame implements ActionListener {
+    JPanel SongInfoWindow;
+    JButton b, back;
+    JLabel Search, Back;
     JTextField Input;
     Font font;
     JComboBox<String> cb;
     Connection con;
     Hashtable<String, String> userdata;
+    String Object[][] = {
+            { "", "", "", "", "" },
+            { "", "", "", "", "" },
+            { "", "", "", "", "" },
+            { "", "", "", "", "" },
+            { "", "", "", "", "" },
+            { "", "", "", "", "" },
+            { "", "", "", "", "" },
+            { "", "", "", "", "" },
+            { "", "", "", "", "" },
+            { "", "", "", "", "" },
+            { "", "", "", "", "" },
+            { "", "", "", "", "" },
+            { "", "", "", "", "" },
+            { "", "", "", "", "" },
+            { "", "", "", "", "" },
+            { "", "", "", "", "" },
+            { "", "", "", "", "" },
+    };
+    String clm[] = {
+            "", "", "", "", "" };
 
     viewDetail(Connection con, Hashtable<String, String> userdata) {
         this.con = con;
         this.userdata = userdata;
         setTitle("Music Recording Management System");
-        
+
         SongInfoWindow = new JPanel();
         this.getContentPane().add(SongInfoWindow);
         SongInfoWindow.setLayout(null);
-        SongInfoWindow.setBackground(new Color(68,67,68));
+        SongInfoWindow.setBackground(new Color(68, 67, 68));
         SongInfoWindow.setBounds(0, 0, 400, 400);
         this.setBounds(100, 100, 564, 450);
-        
+
         Search = new JLabel("Search by:");
         Back = new JLabel("Back");
         b = new JButton("Search");
         back = new JButton("Back");
         Input = new JTextField();
-        
-        font  = new Font(Font.SANS_SERIF,  Font.BOLD, 10);
+
+        font = new Font(Font.SANS_SERIF, Font.BOLD, 10);
 
         String Options[] = { "Song Name", "Artist Name", "Album Name" };
         cb = new JComboBox<String>(Options);
         Search.setBounds(10, 50, 100, 30);
         Search.setForeground(Color.WHITE);
         cb.setBounds(100, 50, 200, 30);
-        Input.setBounds(315, 50,165, 30);
+        Input.setBounds(315, 50, 165, 30);
         b.setBounds(265, 100, 100, 20);
         b.addActionListener(this);
         back.setBounds(140, 100, 100, 20);
         back.addActionListener(this);
 
-//display the data in the tabular format insde output text field
-    
-        
-        String Object [][] ={
-                {"sef", "eff", null, null,null,null},
-                {null, null, null, null,null,null},
-                {null, null, null, null,null,null},
-                {null, null, null, null,null,null},
-                {null, null, null, null,null,null},
-                {null, null, null, null,null,null},
-                {null, null, null, null,null,null},
-                {null, null, null, null,null,null},
-                {null, null, null, null,null,null},
-                {null, null, null, null,null,null},
-                {null, null, null, null,null,null},
-                {null, null, null, null,null,null},
-                {null, null, null, null,null,null},
-                {null, null, null, null,null,null},
-                {null, null, null, null,null,null},
-                {null, null, null, null,null,null},
-                {null, null, null, null,null,null},
-            };
-        String clm []= {
-                "Title 1", "Title 2", "Title 3", "Title 4", "Title 5", "Title 6"};
-        JTable jt= new JTable(Object,clm);
-        jt.setBounds(10, 150,400, 200);
-        jt.setEnabled(false);
-        jt.setVisible(true);
-        jt.setAutoResizeMode(JTable.AUTO_RESIZE_OFF); 
-        JScrollPane pane = new JScrollPane(jt,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        
-        pane.setBounds(10, 150,469, 200);
-        SongInfoWindow.add(pane);
         SongInfoWindow.add(Search);
         SongInfoWindow.add(cb);
         SongInfoWindow.add(Input);
@@ -87,13 +76,13 @@ public class viewDetail extends JFrame implements ActionListener{
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
     public void actionPerformed(ActionEvent ae) {
-        String option=cb.getItemAt(cb.getSelectedIndex());  
-        String input = Input.getText();
+        String option = cb.getItemAt(cb.getSelectedIndex());
         if (ae.getSource() == back) {
             this.dispose();
             try {
-                Home form = new Home(con,userdata);
+                Home form = new Home(con, userdata);
                 form.setVisible(true);
                 form.invalidate();
                 form.validate();
@@ -101,22 +90,179 @@ public class viewDetail extends JFrame implements ActionListener{
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(null, e.getMessage());
             }
-        }
-        
+        } else if (option.equals("Song Name")) {
+            String song_name_input = Input.getText();
+            try {
+                String sql = "SELECT song_name,first_name as artist,albumname,duration,release_date FROM song_info INNER JOIN artist_info ON song_info.artist = artist_info.artist_id INNER JOIN album_info ON song_info.album = album_info.album_id WHERE song_name= ?";
+                PreparedStatement pst = con.prepareStatement(sql);
+                pst.setString(1, song_name_input);
+                ResultSet rs = pst.executeQuery();
+                int r = 0;
+                for (int i = 0; i < clm.length; i++) {
+                    clm[i] = "";
+                }
+                for (int i = 0; i < Object.length; i++) {
+                    for (int j = 0; j < Object[i].length; j++) {
+                        Object[i][j] = "";
+                    }
+                }
+                clm[0] = "Song Name";
+                clm[1] = "Artist Name";
+                clm[2] = "Album Name";
+                clm[3] = "Duration";
+                clm[4] = "Release Date";
+                while (rs.next()) {
+                    Object[r][0] = rs.getString("song_name");
+                    Object[r][1] = rs.getString("artist");
+                    Object[r][2] = rs.getString("albumname");
+                    Object[r][3] = rs.getString("duration");
+                    Object[r][4] = rs.getString("release_date");
+                    r++;
+                }
+                if(Object[0][0]==""){
+                    JOptionPane.showMessageDialog(null, "No such song found");
+                    return;
+                }
+                JTable jt = new JTable(Object, clm);
+                jt.setBounds(10, 150, 400, 200);
+                jt.setEnabled(false);
+                jt.setVisible(true);
+                jt.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+                JScrollPane pane = new JScrollPane(jt, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                        JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                pane.setBounds(10, 150, 469, 200);
+                SongInfoWindow.add(pane);
+                SongInfoWindow.add(Search);
+                SongInfoWindow.add(cb);
+                SongInfoWindow.add(Input);
+                SongInfoWindow.add(b);
+                SongInfoWindow.add(back);
+                add(SongInfoWindow, BorderLayout.CENTER);
+                setVisible(true);
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // JDBC CONNECTION HERE
-        /*
-        [TODO]:
-        1. Connect to the database
-        2.1.a Search for the song
-        2.1.b If found, display the song details
-        2.1.c If not found, display "No such song found"
-        2.2.a Search for the artist
-        2.2.b If found, display the artist details
-        2.2.c If not found, display "No such artist found"
-        2.3.a Search for the album
-        2.3.b If found, display the album details
-        2.3.c If not found, display "No such album found"
-        */
+            } catch (Exception e) {
+
+                System.out.println(e.getMessage());
+            }
+        }
+
+        else if (option.equals("Artist Name")) {
+            String artist_name_input = Input.getText();
+            try {
+                String sql = "SELECT song_name,first_name as artist,albumname,duration,release_date FROM song_info INNER JOIN artist_info ON song_info.artist = artist_info.artist_id INNER JOIN album_info ON song_info.album = album_info.album_id WHERE first_name= ?";
+                PreparedStatement pst = con.prepareStatement(sql);
+                pst.setString(1, artist_name_input);
+                ResultSet rs = pst.executeQuery();
+                int r = 0;
+                for (int i = 0; i < clm.length; i++) {
+                    clm[i] = "";
+                }
+                for (int i = 0; i < Object.length; i++) {
+                    for (int j = 0; j < Object[i].length; j++) {
+                        Object[i][j] = "";
+                    }
+                }
+                clm[0] = "Song Name";
+                clm[1] = "Artist Name";
+                clm[2] = "Album Name";
+                clm[3] = "Duration";
+                clm[4] = "Release Date";
+                while (rs.next()) {
+                    Object[r][0] = rs.getString("song_name");
+                    Object[r][1] = rs.getString("artist");
+                    Object[r][2] = rs.getString("albumname");
+                    Object[r][3] = rs.getString("duration");
+                    Object[r][4] = rs.getString("release_date");
+                    r++;
+                }
+                if(Object[0][0]==""){
+                    JOptionPane.showMessageDialog(null, "No such song found");
+                    return;
+                }
+                JTable jt = new JTable(Object, clm);
+                jt.setBounds(10, 150, 400, 200);
+                jt.setEnabled(false);
+                jt.setVisible(true);
+                jt.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+                JScrollPane pane = new JScrollPane(jt, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                        JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                pane.setBounds(10, 150, 469, 200);
+                // reset song info window
+                // SongInfoWindow = new JPanel();
+                SongInfoWindow.add(pane);
+                SongInfoWindow.add(Search);
+                SongInfoWindow.add(cb);
+                SongInfoWindow.add(Input);
+                SongInfoWindow.add(b);
+                SongInfoWindow.add(back);
+                add(SongInfoWindow, BorderLayout.CENTER);
+                setVisible(true);
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            } catch (Exception e) {
+
+                System.out.println(e.getMessage());
+            }
+        }
+
+        else if (option.equals("Album Name")) {
+            String albumname_input = Input.getText();
+            try {
+                String sql = "SELECT song_name,first_name as artist,albumname,duration,release_date FROM song_info INNER JOIN artist_info ON song_info.artist = artist_info.artist_id INNER JOIN album_info ON song_info.album = album_info.album_id WHERE albumname=?";
+                PreparedStatement pst = con.prepareStatement(sql);
+                pst.setString(1, albumname_input);
+                ResultSet rs = pst.executeQuery();
+                int r = 0;
+                for (int i = 0; i < clm.length; i++) {
+                    clm[i] = "";
+                }
+                for (int i = 0; i < Object.length; i++) {
+                    for (int j = 0; j < Object[i].length; j++) {
+                        Object[i][j] = "";
+                    }
+                }
+                clm[0] = "Song Name";
+                clm[1] = "Artist Name";
+                clm[2] = "Album Name";
+                clm[3] = "Duration";
+                clm[4] = "Release Date";
+
+                while (rs.next()) {
+                    Object[r][0] = rs.getString("song_name");
+                    Object[r][1] = rs.getString("artist");
+                    Object[r][2] = rs.getString("albumname");
+                    Object[r][3] = rs.getString("duration");
+                    Object[r][4] = rs.getString("release_date");
+                    r++;
+                }
+                if(Object[0][0]==""){
+                    JOptionPane.showMessageDialog(null, "No such song found");
+                    return;
+                }
+                
+                JTable jt = new JTable(Object, clm);
+                jt.setBounds(10, 150, 400, 200);
+                jt.setEnabled(false);
+                jt.setVisible(true);
+                jt.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+                JScrollPane pane = new JScrollPane(jt, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                        JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                pane.setBounds(10, 150, 469, 200);
+                SongInfoWindow.add(pane);
+                SongInfoWindow.add(Search);
+                SongInfoWindow.add(cb);
+                SongInfoWindow.add(Input);
+                SongInfoWindow.add(b);
+                SongInfoWindow.add(back);
+                add(SongInfoWindow, BorderLayout.CENTER);
+                setVisible(true);
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            } catch (Exception e) {
+
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
